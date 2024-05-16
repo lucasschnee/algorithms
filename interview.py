7cda738e9aecff9b0e31fd09c54c9eccbd7dc4bb
Lucas Schnee <lucas.j.schnee@vanderbilt.edu>
2024-05-16 08:18:13 -0500

    Update interview.py

--- interview.py
+++ interview.py
@@ -783,4 +783,58 @@ class UnionFind:
             
         self.count -= 1 
         return True 
+	    
 
+class BinaryLifting:
+    def __init__(self, n, edges):
+        self.n = n
+        self.edges = edges
+        self.LOG = n.bit_length()
+        self.graph = defaultdict(list)
+        self.parent = [[-1] * n for _ in range(self.LOG)]
+        self.depth = [0] * n
+        self._build_graph()
+        self._preprocess()
+
+    def _build_graph(self):
+        for u, v in self.edges:
+            self.graph[u].append(v)
+            self.graph[v].append(u)
+
+    def _dfs(self, node, par):
+        self.parent[0][node] = par
+        for neighbor in self.graph[node]:
+            if neighbor == par:
+                continue
+            self.depth[neighbor] = self.depth[node] + 1
+            self._dfs(neighbor, node)
+
+    def _preprocess(self):
+        # Initialize depth and parent array
+        self._dfs(0, -1)
+        # Binary lifting preprocessing
+        for i in range(1, self.LOG):
+            for j in range(self.n):
+                if self.parent[i-1][j] != -1:
+                    self.parent[i][j] = self.parent[i-1][self.parent[i-1][j]]
+
+    def get_kth_ancestor(self, node, k):
+        for i in range(self.LOG):
+            if k & (1 << i):
+                node = self.parent[i][node]
+                if node == -1:
+                    break
+        return node
+
+    def lca(self, u, v):
+        if self.depth[u] < self.depth[v]:
+            u, v = v, u
+        diff = self.depth[u] - self.depth[v]
+        u = self.get_kth_ancestor(u, diff)
+        if u == v:
+            return u
+        for i in range(self.LOG - 1, -1, -1):
+            if self.parent[i][u] != self.parent[i][v]:
+                u = self.parent[i][u]
+                v = self.parent[i][v]
+        return self.parent[0][u]
