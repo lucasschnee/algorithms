3c59f577e62a8a78daf669d888020158f8f89f68
Lucas Schnee <lucas.j.schnee@vanderbilt.edu>
2024-05-18 10:04:53 -0500

    Update interview.py

--- interview.py
+++ interview.py
@@ -879,3 +879,29 @@ class BIT:
             k += k & -k
 
 
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    merged = []
+    l, r = 0, 0
+
+    while l < len(left) and r < len(right):
+        if left[l] <= right[r]:
+            merged.append(left[l])
+            l += 1
+        else:
+            merged.append(right[r])
+            r += 1
+
+    merged.extend(left[l:])
+    merged.extend(right[r:])
+
+    return merged
