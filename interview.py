c9a32b615af8963d86cb87449bd04cb5cbcf082e
Lucas Schnee <lucas.j.schnee@vanderbilt.edu>
2024-06-22 09:24:07 -0500

    Update interview.py

--- interview.py
+++ interview.py
@@ -1044,35 +1044,38 @@ def lca(node):
 
 
 class TrieNode:
-
-    def __init__(self, is_word=False):
-        self.is_word = is_word
-        self.children = [None] * 26
+    def __init__(self):
+        self.is_word = False
+        self.children = {}
 
 class Trie:
-
     def __init__(self):
         self.root = TrieNode()
 
     def insert(self, word: str) -> None:
         node = self.root
         for c in word:
-            if not node.children[ord(c) - ord('a')]:
-                node.children[ord(c) - ord('a')] = TrieNode()
-		    
-            node = node.children[ord(c) - ord('a')]
-
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
         node.is_word = True
 
     def search(self, word: str) -> bool:
-        
         node = self.root
         for c in word:
-            if not node.children[ord(c) - ord('a')]:
+            if c not in node.children:
                 return False
-            node = node.children[ord(c) - ord('a')]
-
+            node = node.children[c]
         return node.is_word
+
+    def starts_with(self, prefix: str) -> bool:
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
+
 
 
 class BIT:
