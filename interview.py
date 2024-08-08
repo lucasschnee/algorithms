5e26c33db60f6042941525d1a4a3537f5f1c64cf
Lucas Schnee <lucas.j.schnee@vanderbilt.edu>
2024-08-08 08:41:15 -0500

    Update interview.py

--- interview.py
+++ interview.py
@@ -674,12 +674,13 @@ B.rangeQuery(l, r)
 B.incr(l, r)
 
 
-# divisors, factors
-divisors = set()
-for i in range(1, ceil(n**0.5) + 1): 
-    if n % i == 0: 
-	divisors.add(i)   
-	divisors.add(n // i)  
+def get_divisors(n):
+	# divisors, factors
+	divisors = set()
+	for i in range(1, ceil(n**0.5) + 1): 
+	    if n % i == 0: 
+		divisors.add(i)   
+		divisors.add(n // i)  
 
 
 # primes
