ce45824c0e4c3e2c9364d81f436b96f50cf93347
Lucas Schnee <lucas.j.schnee@vanderbilt.edu>
2024-12-23 17:38:59 -0600

    Update interview.py

--- interview.py
+++ interview.py
@@ -388,16 +388,10 @@ def z_function(s):
 
 def is_prime(n):
     if n <= 1:
-	return False
-    if n <= 3:
-	return True
-    if n % 2 == 0 or n % 3 == 0:
-	return False
-    i = 5
-    while i * i <= n:
-	if n % i == 0 or n % (i + 2) == 0:
-	    return False
-	i += 6
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
     return True
 
 class UnionFind:
