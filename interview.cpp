4928ae31c37a5106080e03f374b8f2288e2c94cf
Lucas Schnee <lschnee011@gmail.com>
2026-04-07 19:03:19 -0400

    Update interview.cpp

--- interview.cpp
+++ interview.cpp
@@ -41,3 +41,48 @@ invfact[N] = modpow(fact[N], MOD - 2);
 for (int i = N-1; i >= 0; --i) {
    invfact[i] = invfact[i + 1] * (i + 1) % MOD;
 }
+
+
+class UnionFind {
+public:
+    int count;
+    vector<int> parent;
+    vector<int> rank;   
+
+    UnionFind(int N) {
+        count = N;
+        parent.resize(N);
+        rank.assign(N, 1);
+
+        for (int i = 0; i < N; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int p) {
+        if (p != parent[p]) {
+            parent[p] = find(parent[p]);  
+        }
+        return parent[p];
+    }
+
+    bool unite(int p, int q) {
+        int prt = find(p);
+        int qrt = find(q);
+
+        if (prt == qrt) return false;
+
+      
+        if (rank[prt] >= rank[qrt]) {
+            parent[qrt] = prt;
+            rank[prt] += rank[qrt];
+        } else {
+            parent[prt] = qrt;
+            rank[qrt] += rank[prt];
+        }
+
+        count--;
+        return true;
+    }
+};
+
