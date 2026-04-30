489f59867527c3a0ab421eb4a9dc96206733bbaf
Lucas Schnee <lschnee011@gmail.com>
2026-04-30 09:30:39 -0400

    Update interview.cpp

--- interview.cpp
+++ interview.cpp
@@ -277,3 +277,43 @@ public:
         return res;
     }
 };
+
+
+class SegmentTree {
+    int n;
+    vector<int> tree;
+
+public:
+    SegmentTree(int size) {
+        n = size;
+        tree.assign(4 * n, 0);
+    }
+
+    void update(int idx, int val) {
+        update(1, 0, n - 1, idx, val);
+    }
+
+    int query(int l, int r) {
+        return query(1, 0, n - 1, l, r);
+    }
+
+private:
+    void update(int node, int start, int end, int idx, int val) {
+        if (start == end) {
+            tree[node] = val;
+            return;
+        }
+        int mid = (start + end) / 2;
+        if (idx <= mid) update(2 * node, start, mid, idx, val);
+        else update(2 * node + 1, mid + 1, end, idx, val);
+        tree[node] = tree[2 * node] + tree[2 * node + 1];
+    }
+
+    int query(int node, int start, int end, int l, int r) {
+        if (r < start || end < l) return 0;
+        if (l <= start && end <= r) return tree[node];
+        int mid = (start + end) / 2;
+        return query(2 * node, start, mid, l, r) + query(2 * node + 1, mid + 1, end, l, r);
+    }
+};
+
